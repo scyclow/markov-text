3bb052825f27b696782623b3a30772b89fbe9d85
scyclow <spike716@gmail.com>
2016-05-18 23:25:25 -0400

    added interface

--- markovText.js
+++ markovText.js
@@ -1,8 +1,8 @@
-var inp = `
-Modern NLP algorithms are based on machine learning, especially statistical machine learning. The paradigm of machine learning is different from that of most prior attempts at language processing. Prior implementations of language-processing tasks typically involved the direct hand coding of large sets of rules. The machine-learning paradigm calls instead for using general learning algorithms — often, although not always, grounded in statistical inference — to automatically learn such rules through the analysis of large corpora of typical real-world examples. A corpus (plural, "corpora") is a set of documents (or sometimes, individual sentences) that have been hand-annotated with the correct values to be learned.
+// var inp = `
+// Modern NLP algorithms are based on machine learning, especially statistical machine learning. The paradigm of machine learning is different from that of most prior attempts at language processing. Prior implementations of language-processing tasks typically involved the direct hand coding of large sets of rules. The machine-learning paradigm calls instead for using general learning algorithms — often, although not always, grounded in statistical inference — to automatically learn such rules through the analysis of large corpora of typical real-world examples. A corpus (plural, "corpora") is a set of documents (or sometimes, individual sentences) that have been hand-annotated with the correct values to be learned.
 
-Many different classes of machine learning algorithms have been applied to NLP tasks. These algorithms take as input a large set of "features" that are generated from the input data. Some of the earliest-used algorithms, such as decision trees, produced systems of hard if-then rules similar to the systems of hand-written rules that were then common. Increasingly, however, research has focused on statistical models, which make soft, probabilistic decisions based on attaching real-valued weights to each input feature. Such models have the advantage that they can express the relative certainty of many different possible answers rather than only one, producing more reliable results when such a model is included as a component of a larger system.
-`
+// Many different classes of machine learning algorithms have been applied to NLP tasks. These algorithms take as input a large set of "features" that are generated from the input data. Some of the earliest-used algorithms, such as decision trees, produced systems of hard if-then rules similar to the systems of hand-written rules that were then common. Increasingly, however, research has focused on statistical models, which make soft, probabilistic decisions based on attaching real-valued weights to each input feature. Such models have the advantage that they can express the relative certainty of many different possible answers rather than only one, producing more reliable results when such a model is included as a component of a larger system.
+// `
 // if word ends a sentence
 var isLastWord = word => (
   word[word.length-1] === '.' ||
@@ -11,9 +11,11 @@ var isLastWord = word => (
 var sample = arr => arr[Math.floor(Math.random() * arr.length)];
 var capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
 
-function markov(input) {
+function markov(input, limit) {
   var tokenized = input.split(' ');
   var textLen = tokenized.length;
+  limit = limit || textLen;
+
   var nextWords = {};
 
   var startingWords = [];
@@ -38,7 +40,7 @@ function markov(input) {
   var output = [capitalize(sample(startingWords))]
 
   var previousWord;
-  for (var i = 1; i < textLen; i++) {
+  for (var i = 1; i < limit; i++) {
     previousWord = output[i-1];
     var nextWord = nextWords[previousWord] && sample(nextWords[previousWord])
 
@@ -64,5 +66,3 @@ function markov(input) {
 
   return output.join(' ')
 }
-
-console.log(markov(inp))
